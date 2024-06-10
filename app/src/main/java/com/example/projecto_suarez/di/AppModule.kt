4c417107701d1c53d3fa5fb4690c17b55b5d2e78
Boris Giovanni <borisgiovanni2002@gmail.com>
2024-06-10 12:44:23 -0500

    deleted files

--- app/src/main/java/com/example/projecto_suarez/di/AppModule.kt
+++ app/src/main/java/com/example/projecto_suarez/di/AppModule.kt
@@ -20,7 +20,6 @@ import com.example.projecto_suarez.domain.usescases.news.SearchNews
 import com.example.projecto_suarez.domain.usescases.news.SelectArticle
 import com.example.projecto_suarez.domain.usescases.news.SelectArticles
 import com.example.projecto_suarez.domain.usescases.news.UpsertArticle
-import com.example.projecto_suarez.services.BeaconReference
 import com.example.projecto_suarez.services.BeaconScanner
 import com.example.projecto_suarez.util.Constants.BASE_URL
 import com.example.projecto_suarez.util.Constants.NEWS_DATABASE_NAME
@@ -30,7 +29,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import javax.inject.Singleton
 
 @Module
@@ -102,11 +100,6 @@ object AppModule {
     fun provideNewsDao(
         newsDatabase: NewsDatabase
     ) : NewsDao = newsDatabase.newsDao
-    @Provides
-    @Singleton
-    fun provideBeaconReference(
-        application: Application
-    ) : BeaconReference = BeaconReference(application)
 
     @Provides
     @Singleton
--- app/src/main/java/com/example/projecto_suarez/presentation/map/BeaconViewModel.kt
+++ app/src/main/java/com/example/projecto_suarez/presentation/map/BeaconViewModel.kt
@@ -1,16 +1,10 @@
 package com.example.projecto_suarez.presentation.map
 
-import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
-import com.example.projecto_suarez.domain.usescases.news.NewsUseCases
-import org.altbeacon.beacon.*
-import com.example.projecto_suarez.services.BeaconReference
 import com.example.projecto_suarez.services.BeaconScanner
-import com.example.projecto_suarez.util.BeaconLibrary.BleScanCallback
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 @HiltViewModel
