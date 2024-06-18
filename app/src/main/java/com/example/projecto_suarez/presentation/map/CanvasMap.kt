b674f8cd03cbe1f4563424fc2479ca9ca2e8c46d
fdelgadov <fdelgadov@unsa.edu.pe>
2024-06-18 18:03:12 -0500

    Boton de ubicacion aleatoria

--- app/src/main/java/com/example/projecto_suarez/presentation/map/CanvasMap.kt
+++ app/src/main/java/com/example/projecto_suarez/presentation/map/CanvasMap.kt
@@ -1,77 +1,90 @@
 package com.example.projecto_suarez.presentation.map
 
-import android.graphics.Point
+import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.withTransform
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
-fun Map() {
+fun CanvasMap() {
+    val TAG = "MAP"
     var point by remember { mutableStateOf(Pair(0f, 0f)) }
+    var maxCanvasW = 0f
+    var maxCanvasH = 0f
 
-    BoxWithConstraints {
-        Canvas(
-            modifier = Modifier
-                .size(maxWidth)
-                .padding(10.dp)) {
+    Column {
+        BoxWithConstraints {
+            Canvas(
+                modifier = Modifier
+                    .size(maxWidth)
+                    .padding(10.dp)
+                    .pointerInput(Unit) {
+                        detectTapGestures { offset ->
+                            Log.d(TAG, "Se toca la pantalla en ${offset}")
+                        }
+                    }){
 
-            var path = Path().apply {
-                moveTo(0f, 0f)
-                lineTo(size.width, 0f)
-                lineTo(size.width, size.height)
-                lineTo(0f, size.height)
-                close()
-            }
+                maxCanvasH = size.width
+                maxCanvasW = size.height
 
-            drawPath(
-                path = path,
-                color = Color.LightGray,
-                style = Stroke(width = 5.dp.toPx())
-            )
+                var path = Path().apply {
+                    moveTo(0f, 0f)
+                    lineTo(size.width, 0f)
+                    lineTo(size.width, size.height)
+                    lineTo(0f, size.height)
+                    close()
+                }
 
-            path = Path().apply {
-                moveTo(0f, size.height*.5f)
-                lineTo(0f, size.height*(.5f-.15f))
+                drawPath(
+                    path = path,
+                    color = Color.LightGray,
+                    style = Stroke(width = 5.dp.toPx())
+                )
 
-                close()
-            }
+                path = Path().apply {
+                    moveTo(0f, size.height*.5f)
+                    lineTo(0f, size.height*(.5f-.15f))
 
-            drawPath(
-                path = path,
-                color = Color.Black,
-                style = Stroke(width = 5.dp.toPx())
-            )
+                    close()
+                }
 
-            point?.let { (x, y) ->
-                drawPoint(x, y, Color.Red)
+                drawPath(
+                    path = path,
+                    color = Color.Black,
+                    style = Stroke(width = 5.dp.toPx())
+                )
+
+                point?.let { (x, y) ->
+                    drawPoint(x, y, Color.Red)
+                }
             }
         }
+        Button(onClick = { point = Pair((Math.random()*maxCanvasW).toFloat(), (Math.random()*maxCanvasH).toFloat()) }) {
+            Text("Mi ubicación")
+        }
     }
+
 }
 
 fun DrawScope.drawPoint(x: Float, y: Float, color: Color) {
