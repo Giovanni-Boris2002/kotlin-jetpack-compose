49118c350234f3f80480bcdc4f7464982420438b
fdelgadov <fdelgadov@unsa.edu.pe>
2024-06-19 20:44:02 -0500

    feat: se representan las pinturas con rectangulos rojos
    - se agrega al mapa las pinturas representadas por rectangulos rojos
    - cada pintura tiene asignado un id
    - cada pintura lanza un evento al ser clickeada

--- app/src/main/java/com/example/projecto_suarez/presentation/map/CanvasMap.kt
+++ app/src/main/java/com/example/projecto_suarez/presentation/map/CanvasMap.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -17,18 +19,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+
+val TAG = "MAP"
 
 @Preview
 @Composable
 fun CanvasMap() {
-    val TAG = "MAP"
     var point by remember { mutableStateOf(Pair(0f, 0f)) }
     var maxCanvasW = 0f
     var maxCanvasH = 0f
@@ -45,8 +51,8 @@ fun CanvasMap() {
                         }
                     }){
 
-                maxCanvasH = size.width
-                maxCanvasW = size.height
+                maxCanvasW = size.width
+                maxCanvasH = size.height
 
                 var path = Path().apply {
                     moveTo(0f, 0f)
@@ -79,6 +85,10 @@ fun CanvasMap() {
                     drawPoint(x, y, Color.Red)
                 }
             }
+            picture(1, maxWidth*.1f, maxHeight*.1f, maxWidth, maxHeight)
+            picture(2, maxWidth*.3f, maxHeight*.1f, maxWidth, maxHeight)
+            picture(3, maxWidth*.5f, maxHeight*.1f, maxWidth, maxHeight)
+            picture(4, maxWidth*.7f, maxHeight*.1f, maxWidth, maxHeight)
         }
         Button(onClick = { point = Pair((Math.random()*maxCanvasW).toFloat(), (Math.random()*maxCanvasH).toFloat()) }) {
             Text("Mi ubicación")
@@ -93,4 +103,22 @@ fun DrawScope.drawPoint(x: Float, y: Float, color: Color) {
         center = Offset(x, y),
         radius = 5.dp.toPx()
     )
+}
+
+@Composable
+fun picture(ID: Int, x: Dp, y: Dp, w: Dp, h: Dp) {
+    Canvas(
+        modifier = Modifier
+            .size(w*.1f)
+            .offset(x, y)
+            .pointerInput(Unit) {
+                detectTapGestures { offset ->
+                    Log.d(TAG, "Se toca la pintura ${ID} en ${offset}")
+                }
+            }
+    ){
+        drawRect(
+            color = Color.Red
+        )
+    }
 }
